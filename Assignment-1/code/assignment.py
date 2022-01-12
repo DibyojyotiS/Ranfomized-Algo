7f3796cf8a36a35a989ea48aa1006a230eaafec3
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-01-12 13:51:51 +0530

    phew!

--- Assignment-1/code/assignment.py
+++ Assignment-1/code/assignment.py
@@ -1,16 +1,16 @@
 # timer for benchmarking
 import sys
 import json
+from collections import defaultdict
 from numpy.random import uniform # rnd generator
 from time import perf_counter_ns as timer
 
 from deterministic_algos import QuickSort, MergeSort
 from randomied_algos import RandomizedQuickSort
 
-# question-1
-# Randomized Quick-Sort v/s Quick-Sort
-def Qusetion1():
+def assignment():
 
+    # this is no longer required
     sys.setrecursionlimit(10**7)
 
     def generate_randomlist(n=10E6):
@@ -35,46 +35,80 @@ def Qusetion1():
     def incrementCounter(counter, data):
         for i,d in enumerate(data):
             counter[i] += d
-        return  
+        return
 
-    def runExperiemnt(sortingAlgo, list_n, K):
+    def save_json(filname, data):
+        with open(filname, 'w') as f:
+            json.dump(data, f)
+
+    def runExperiemnt(sortingAlgo, list_n, K, filename=None):
         # will run experiments for the following list of n
-        # each experiemnt will be repeated about K times
+        # each experiemnt will be repeated K times
 
         print('running for', sortingAlgo.__name__)
         
-        avgtimes = []
+        data = {
+            'array-size': [], # list of floats/ints
+            'avg-comparisons': [], # list of floats
+            'avg-single-sort-time-ns': [], # list of floats
+            'avg-double-sort-time-ns': [], # list of floats
+            'raw-samples':[] # list of dicts
+        }
+
         # get sorting times
         for n in list_n:
             arr = generate_randomlist(n) 
-            print(n)
-            # accumulators
+            print(f'{n} : 0%', end='')
+            # accumulators - stores the average
             counters = [0,0,0] #conparisons, single_t, double_t
+            samples = {'comparisons':[], 
+                        'single-sort-time-ns':[], 
+                        'double-sort-time-ns':[]}
             # run K times 
             for k in range(K):
                 # for deterministc-quicksort
                 numcomparisons, single_sort_t = time_single_sort(sortingAlgo, arr)
                 double_sort_t = time_double_sort(sortingAlgo, arr)
-                incrementCounter(counters, 
-                                (numcomparisons/K, single_sort_t/K, double_sort_t/K))
+                samples['comparisons'].append(numcomparisons)
+                samples['single-sort-time-ns'].append(single_sort_t)
+                samples['double-sort-time-ns'].append(double_sort_t)
+                sample = (numcomparisons, single_sort_t, double_sort_t)
+                incrementCounter(counters, [x/K for x in sample])
+                print(f'\r{n} : {100*(k+1)/K:.2f}%', end='')
+            data['array-size'].append(n)
+            data['raw-samples'].append(samples)
+            data['avg-comparisons'].append(counters[0])
+            data['avg-single-sort-time-ns'].append(counters[1])
+            data['avg-double-sort-time-ns'].append(counters[2])
+            print()
 
-            avgtimes.append(counters)
-        return avgtimes
+        if filename:
+            save_json(filename, data)
+        return data
 
-    # print results
-    list_n = [10**x for x in range(2,5)]
-    avgtimes_QuickSort = runExperiemnt(QuickSort, list_n, K=500)
-    print(avgtimes_QuickSort)
+    # run experiemtns and results
+    list_n = [10**x for x in range(2,7)]
+    avgtimes_RandomizedQuickSort = runExperiemnt(RandomizedQuickSort, list_n, K=500, 
+                                        filename='randomized_quickSort_data.json')
+    print('rand-quick-sort:\n', avgtimes_RandomizedQuickSort['avg-comparisons'],
+                                avgtimes_RandomizedQuickSort['avg-single-sort-time-ns'],
+                                avgtimes_RandomizedQuickSort['avg-double-sort-time-ns'])
 
     list_n = [10**x for x in range(2,7)]
-    avgtimes_RandomizedQuickSort = runExperiemnt(RandomizedQuickSort, list_n, K=500)
-    print(avgtimes_RandomizedQuickSort)
+    avgtimes_MergeSort = runExperiemnt(MergeSort, list_n, K=500, 
+                                        filename='mergeSort_data.json')
+    print('merge-sort:\n', avgtimes_MergeSort['avg-comparisons'],
+                            avgtimes_MergeSort['avg-single-sort-time-ns'],
+                            avgtimes_MergeSort['avg-double-sort-time-ns'])
 
-    # save stuffs
-    data = {'avgtimes_QuickSort': avgtimes_QuickSort,
-            'avgtimes_RandomizedQuickSort': avgtimes_RandomizedQuickSort}
-    with open('save.json', 'w') as f:
-        json.dump(data, f)
+    list_n = [10**x for x in range(2,5)]
+    avgtimes_QuickSort = runExperiemnt(QuickSort, list_n, K=500, 
+                                        filename='quickSort_data.json')
+    print('quick-sort:\n', avgtimes_QuickSort['avg-comparisons'],
+                            avgtimes_QuickSort['avg-single-sort-time-ns'],
+                            avgtimes_QuickSort['avg-double-sort-time-ns'])
+    
+    return 'done'
 
 if __name__ == "__main__":
-    Qusetion1()
+    assignment()
